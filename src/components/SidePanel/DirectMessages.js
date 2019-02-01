8d3a621e99f3bbca1b9ca8017f9b07b892344e85
kstulgys <karolis.stulgys@gmail.com>
2019-02-01 14:17:47 +1100

    wip

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,135 +1,135 @@
-import React, { useState, useEffect } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
-import useChannel from '../../store/Channel';
+// import React, { useState, useEffect } from 'react';
+// import { Menu, Icon } from 'semantic-ui-react';
+// import useChannel from '../../store/Channel';
 
-export default function DirectMessages() {
-  const [channel, { createChannel, changeChannel }] = useChannel();
+// export default function DirectMessages() {
+//   const [channel, { createChannel, changeChannel }] = useChannel();
 
-  // state = {
-  //   activeChannel: "",
-  //   user: this.props.currentUser,
-  //   users: [],
-  //   usersRef: firebase.database().ref("users"),
-  //   connectedRef: firebase.database().ref(".info/connected"),
-  //   presenceRef: firebase.database().ref("presence")
-  // };
+//   // state = {
+//   //   activeChannel: "",
+//   //   user: this.props.currentUser,
+//   //   users: [],
+//   //   usersRef: firebase.database().ref("users"),
+//   //   connectedRef: firebase.database().ref(".info/connected"),
+//   //   presenceRef: firebase.database().ref("presence")
+//   // };
 
-  // componentDidMount() {
-  //   if (this.state.user) {
-  //     this.addListeners(this.state.user.uid);
-  //   }
-  // }
+//   // componentDidMount() {
+//   //   if (this.state.user) {
+//   //     this.addListeners(this.state.user.uid);
+//   //   }
+//   // }
 
-  // componentWillUnmount() {
-  //   this.removeListeners();
-  // }
+//   // componentWillUnmount() {
+//   //   this.removeListeners();
+//   // }
 
-  // removeListeners = () => {
-  //   this.state.usersRef.off();
-  //   this.state.presenceRef.off();
-  //   this.state.connectedRef.off();
-  // };
+//   // removeListeners = () => {
+//   //   this.state.usersRef.off();
+//   //   this.state.presenceRef.off();
+//   //   this.state.connectedRef.off();
+//   // };
 
-  // addListeners = currentUserUid => {
-  //   let loadedUsers = [];
-  //   this.state.usersRef.on("child_added", snap => {
-  //     if (currentUserUid !== snap.key) {
-  //       let user = snap.val();
-  //       user["uid"] = snap.key;
-  //       user["status"] = "offline";
-  //       loadedUsers.push(user);
-  //       this.setState({ users: loadedUsers });
-  //     }
-  //   });
+//   // addListeners = currentUserUid => {
+//   //   let loadedUsers = [];
+//   //   this.state.usersRef.on("child_added", snap => {
+//   //     if (currentUserUid !== snap.key) {
+//   //       let user = snap.val();
+//   //       user["uid"] = snap.key;
+//   //       user["status"] = "offline";
+//   //       loadedUsers.push(user);
+//   //       this.setState({ users: loadedUsers });
+//   //     }
+//   //   });
 
-  // this.state.connectedRef.on("value", snap => {
-  //   if (snap.val() === true) {
-  //     const ref = this.state.presenceRef.child(currentUserUid);
-  //     ref.set(true);
-  //     ref.onDisconnect().remove(err => {
-  //       if (err !== null) {
-  //         console.error(err);
-  //       }
-  //     });
-  //   }
-  // });
+//   // this.state.connectedRef.on("value", snap => {
+//   //   if (snap.val() === true) {
+//   //     const ref = this.state.presenceRef.child(currentUserUid);
+//   //     ref.set(true);
+//   //     ref.onDisconnect().remove(err => {
+//   //       if (err !== null) {
+//   //         console.error(err);
+//   //       }
+//   //     });
+//   //   }
+//   // });
 
-  // this.state.presenceRef.on("child_added", snap => {
-  //   if (currentUserUid !== snap.key) {
-  //     this.addStatusToUser(snap.key);
-  //   }
-  // });
+//   // this.state.presenceRef.on("child_added", snap => {
+//   //   if (currentUserUid !== snap.key) {
+//   //     this.addStatusToUser(snap.key);
+//   //   }
+//   // });
 
-  //   this.state.presenceRef.on("child_removed", snap => {
-  //     if (currentUserUid !== snap.key) {
-  //       this.addStatusToUser(snap.key, false);
-  //     }
-  //   });
-  // };
+//   //   this.state.presenceRef.on("child_removed", snap => {
+//   //     if (currentUserUid !== snap.key) {
+//   //       this.addStatusToUser(snap.key, false);
+//   //     }
+//   //   });
+//   // };
 
-  // addStatusToUser = (userId, connected = true) => {
-  //   const updatedUsers = this.state.users.reduce((acc, user) => {
-  //     if (user.uid === userId) {
-  //       user["status"] = `${connected ? "online" : "offline"}`;
-  //     }
-  //     return acc.concat(user);
-  //   }, []);
-  //   this.setState({ users: updatedUsers });
-  // };
+//   // addStatusToUser = (userId, connected = true) => {
+//   //   const updatedUsers = this.state.users.reduce((acc, user) => {
+//   //     if (user.uid === userId) {
+//   //       user["status"] = `${connected ? "online" : "offline"}`;
+//   //     }
+//   //     return acc.concat(user);
+//   //   }, []);
+//   //   this.setState({ users: updatedUsers });
+//   // };
 
-  // isUserOnline = user => user.status === "online";
+//   // isUserOnline = user => user.status === "online";
 
-  // changeChannel = user => {
-  //   const channelId = this.getChannelId(user.uid);
-  //   const channelData = {
-  //     id: channelId,
-  //     name: user.name
-  //   };
-  //   this.props.setCurrentChannel(channelData);
-  //   this.props.setPrivateChannel(true);
-  //   this.setActiveChannel(user.uid);
-  // };
+//   // changeChannel = user => {
+//   //   const channelId = this.getChannelId(user.uid);
+//   //   const channelData = {
+//   //     id: channelId,
+//   //     name: user.name
+//   //   };
+//   //   this.props.setCurrentChannel(channelData);
+//   //   this.props.setPrivateChannel(true);
+//   //   this.setActiveChannel(user.uid);
+//   // };
 
-  // getChannelId = userId => {
-  //   const currentUserId = this.state.user.uid;
-  //   return userId < currentUserId
-  //     ? `${userId}/${currentUserId}`
-  //     : `${currentUserId}/${userId}`;
-  // };
+//   // getChannelId = userId => {
+//   //   const currentUserId = this.state.user.uid;
+//   //   return userId < currentUserId
+//   //     ? `${userId}/${currentUserId}`
+//   //     : `${currentUserId}/${userId}`;
+//   // };
 
-  // setActiveChannel = userId => {
-  //   this.setState({ activeChannel: userId });
-  // };
+//   // setActiveChannel = userId => {
+//   //   this.setState({ activeChannel: userId });
+//   // };
 
-  // render() {
-  //   const { users, activeChannel } = this.state;
+//   // render() {
+//   //   const { users, activeChannel } = this.state;
 
-  // useEffect(() => {
-  //   subscribeToUsers();
-  // }, []);
+//   // useEffect(() => {
+//   //   subscribeToUsers();
+//   // }, []);
 
-  return (
-    <Menu.Menu className="menu">
-      <Menu.Item>
-        <span>
-          <Icon name="mail" /> DIRECT MESSAGES (0)
-        </span>{' '}
-      </Menu.Item>
-      {message.users.map(user => (
-        <Menu.Item
-          key={user.id}
-          // active={user.uid === activeChannel}
-          onClick={() => changeChannel(user)}
-          style={{ opacity: 0.7, fontStyle: 'italic' }}>
-          <Icon name="circle" color={user.isOnline ? 'green' : 'red'} />@{' '}
-          {user.displayName}
-        </Menu.Item>
-      ))}
-    </Menu.Menu>
-  );
-}
+//   return (
+//     <Menu.Menu className="menu">
+//       <Menu.Item>
+//         <span>
+//           <Icon name="mail" /> DIRECT MESSAGES (0)
+//         </span>{' '}
+//       </Menu.Item>
+//       {message.users.map(user => (
+//         <Menu.Item
+//           key={user.id}
+//           // active={user.uid === activeChannel}
+//           onClick={() => changeChannel(user)}
+//           style={{ opacity: 0.7, fontStyle: 'italic' }}>
+//           <Icon name="circle" color={user.isOnline ? 'green' : 'red'} />@{' '}
+//           {user.displayName}
+//         </Menu.Item>
+//       ))}
+//     </Menu.Menu>
+//   );
+// }
 
-// export default connect(
-//   null,
-//   { setCurrentChannel, setPrivateChannel }
-// )(DirectMessages);
+// // export default connect(
+// //   null,
+// //   { setCurrentChannel, setPrivateChannel }
+// // )(DirectMessages);
